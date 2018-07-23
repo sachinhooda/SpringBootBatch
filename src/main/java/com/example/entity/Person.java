d8c0f9395fa2b825333eb10b31e62bd893f6a51e
Sachin Hooda <sachin.hooda@hotmail.com>
2018-07-23 19:22:34 +0530

    Test Batch processing example

---
+++ src/main/java/com/example/entity/Person.java
@@ -0,0 +1,34 @@
+package com.example.entity;
+
+public class Person {
+
+	private String firstName;
+	private String lastName;
+
+	public Person() {
+		super();
+	}
+
+	public Person(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
